a200370aa119e01614db8ded35611043e99b51b1
gus-imagica <gus@imagica.technology>
2017-05-05 08:56:52 -0700

    Made the spectrum script iterative
    Helps to analyse multiple wavelengths.

--- jazSpect.py
+++ jazSpect.py
@@ -15,30 +15,49 @@ print(devices)
 spec = sb.Spectrometer(devices[0])
 spec.integration_time_micros(1500) # This is the minimum
 spec.minimum_integration_time_micros
-specData = spec.spectrum()
-index_max = np.argmax(specData[1])
-lam_max = specData[0][index_max]
-print("maximum wavelength: ", lam_max)
-plt.figure()
-plt.plot(specData[0], specData[1])
-plt.show()
 
-plt.figure()
-plt.plot(specData[0][index_max-50:index_max+50], specData[1][index_max-50:index_max+50])
-plt.show()
+lambda_vect = []
+spectrum_lambda = spec.wavelengths()
+spectra = []
+maxes = []
 
-# Remove the noise at all wavelengths using median.
-med = np.median(specData[1])
-specData[1] = specData[1]-med
-
-# Find the average data point by bisecting data.
-integ = sum(specData[1])
-halfint = integ/2
-csum = 0
-for x in range(len(specData[1])):
-    csum = csum+specData[1][x]
-    if csum > halfint:
-        print("data average wavelength: ", specData[0][x])
+while True:
+    lambda_in = input("Enter nominal wavelength")
+    if not lambda_in:
         break
+    else:
+        lambda_vect.append(int(lambda_in))
+    specData = spec.intensities()
+    spectra = np.append(spectra,specData)
+    
+    # absolute maximum value
+    index_max = np.argmax(specData)
+    lam_max = spectrum_lambda[index_max]
+    print("maximum wavelength: ", lam_max)
+    
+    # Remove the noise at all wavelengths using median.
+    med = np.median(specData)
+    specData = specData-med
+    
+    # Find the average data point by bisecting data.
+    integ = sum(specData)
+    halfint = integ/2
+    csum = 0
+    for x in range(len(specData)):
+        csum = csum+specData[x]
+        if csum > halfint:
+            lam_bisect = spectrum_lambda[x]
+            print("data average wavelength: ", lam_bisect)
+            break
+    
+    maxes.append([lam_max, lam_bisect])
+    
+    plt.figure()
+    plt.plot(spectrum_lambda, specData)
+    plt.show()
+    
+    plt.figure()
+    plt.plot(spectrum_lambda[index_max-50:index_max+50], specData[index_max-50:index_max+50])
+    plt.show()
 
 spec.close()
